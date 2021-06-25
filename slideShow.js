9706323af3b41e5d1480665b9286fa4c3e29d331
P Thomason <p.thomason@protonmail.com>
2021-06-25 15:09:10 -0400

    first commit

---
+++ slideShow.js
@@ -0,0 +1,32 @@
+// arrows should turn off automatic slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+let timer = setInterval(function () {
+    plusSlides(1);
+}, 3000);
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
